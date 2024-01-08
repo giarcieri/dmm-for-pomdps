a0de02d5e006278453bc1e8a9b34f5c8d6bd28e8
giarcieri <arcieri.1587763@studenti.uniroma1.it>
2024-01-08 14:27:14 +0100

    renamed run_train_dmm_offline.sh

--- run_train_dmm_discrete_offline.sh
+++ run_train_dmm_discrete_offline.sh
@@ -5,25 +5,26 @@
 #SBATCH -n 10
 #SBATCH --time=100:00:00
 #SBATCH --mem-per-cpu=4096
-#SBATCH --job-name=dmm-
-#SBATCH --output=output_1.txt
+#SBATCH --job-name=dmm-disc-oldTr
+#SBATCH --output=output/output_discrete_old_Tr_2.txt
 
 source /cluster/apps/local/env2lmod.sh
 module load gcc/8.2.0 cuda/11.3.1 cudnn/8.2.1.32
 
-python train_dmm_offline.py \
---n_batch 10000 \
+python train_dmm_discrete_offline.py \
+--n_batch 2000 \
 --eps 0.2 \
 --length 100 \
---annealing_epochs 100000 \
---learning-rate 0.0005 \
---learning-rate-decay 1. \
+--annealing_epochs 3000 \
+--learning_rate 0.0001 \
+--learning_rate_decay 1. \
 --beta1 0.9 \
 --beta2 0.999 \
 --emitter_hidden_dim 100 \
 --transition_hidden_dim 100 \
 --inference_hidden_dim 100 \
---num_epochs 5000 \
---mini_batch_size 250 \
+--num_epochs 7000 \
+--mini_batch_size 50 \
 --minimum_annealing_factor 0.0  \
---use-cuda 1
+--use_cuda 1 \
+--simple_env 0
