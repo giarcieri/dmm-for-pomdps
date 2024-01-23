1ff15717fdbc2112b5c6027d462589610d482456
giarcieri <arcieri.1587763@studenti.uniroma1.it>
2024-01-23 19:40:23 +0100

    copied hyperparams from cluster

--- run_train_dmm_discrete_offline.sh
+++ run_train_dmm_discrete_offline.sh
@@ -6,16 +6,16 @@
 #SBATCH --time=100:00:00
 #SBATCH --mem-per-cpu=4096
 #SBATCH --job-name=dmm-disc-oldTr
-#SBATCH --output=output/output_discrete_old_Tr_2.txt
+#SBATCH --output=output/output_discrete_old_Tr.txt
 
 source /cluster/apps/local/env2lmod.sh
 module load gcc/8.2.0 cuda/11.3.1 cudnn/8.2.1.32
 
 python train_dmm_discrete_offline.py \
---n_batch 2000 \
+--n_batch 4000 \
 --eps 0.2 \
 --length 100 \
---annealing_epochs 3000 \
+--annealing_epochs 1000 \
 --learning_rate 0.0001 \
 --learning_rate_decay 1. \
 --beta1 0.9 \
@@ -23,7 +23,7 @@ python train_dmm_discrete_offline.py \
 --emitter_hidden_dim 100 \
 --transition_hidden_dim 100 \
 --inference_hidden_dim 100 \
---num_epochs 7000 \
+--num_epochs 5000 \
 --mini_batch_size 50 \
 --minimum_annealing_factor 0.0  \
 --use_cuda 1 \
